0a6262a060ef8edaea4537470e0beae71bb23400
harris-chris <chrisharriscjh@gmail.com>
2021-10-13 10:41:51 +0900

    First commit

---
+++ default.nix
@@ -0,0 +1,28 @@
+{ pkgs ? import <nixpkgs> {}, vitisPath ? "/opt/xilinx/Vitis_HLS/2021.1" }:
+
+let
+  vitis-fhs = "./nix/vitis-fhs-env.nix";
+
+  vitis-shell-script = pkgs.writeShellScriptBin "vitis-shell" ''
+    #!/bin/bash
+    nix-shell --pure --argstr run "vitis_hls -i" "${vitis-fhs}"
+  '';
+
+  makefile-contents = builtins.readFile ./nix/Makefile;
+  make-script = pkgs.writeShellScriptBin "makeme" ''
+    function run {
+      nix-shell --pure --argstr run "make -f ./nix/Makefile $*" "${vitis-fhs}"
+    }
+
+    if [[ $# -eq 0 ]]; then
+      run
+    else
+      run "$@"
+    fi
+  '';
+
+  shell-scripts = [ vitis-shell-script make-script ];
+
+in pkgs.mkShell {
+  packages = with pkgs; [ gnumake ] ++ shell-scripts;
+}
