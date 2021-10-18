a9feec21797fdfd02232c1034379d59e7633014f
harris-chris <chrisharriscjh@gmail.com>
2021-10-18 11:27:58 +0900

    Add thermostat.vhdl

--- config.mk
+++ config.mk
@@ -1,2 +1,3 @@
 FPGA_TOP = thermostat
+FPGA_PART = xc7a35ticsg324-1L
 SYN_FILES = thermostat.vhdl
---
+++ create_project.tcl
@@ -0,0 +1,5 @@
+create_project -force -part xc7a35ticsg324-1L trttr
+add_files -fileset sources_1 defines.v
+add_files -fileset sources_1 thermostat.vhdl
+add_files -fileset constrs_1 thermostat.xdc
+exit
