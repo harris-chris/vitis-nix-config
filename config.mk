547f8c1de0b9e78b3a15b0dc51373f93648b96d4
harris-chris <chrisharriscjh@gmail.com>
2021-10-18 16:21:04 +0900

    Clearing out

--- config.mk
+++
@@ -1,3 +0,0 @@
-FPGA_TOP = thermostat
-FPGA_PART = xc7a35ticsg324-1L
-SYN_FILES = src/rtl/thermostat.vhdl
--- create_project.tcl
+++
@@ -1,3 +0,0 @@
-create_project -force -part xc7a35ticsg324-1L thermostat
-add_files -fileset sources_1 src/rtl/thermostat.vhdl
-exit
